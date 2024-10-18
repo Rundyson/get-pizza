1964d6c42bf64f874a19ee29c29dcdcb771dfb43
aldrin1122 <0318-1190@lspu.edu.ph>
2024-10-18 13:29:44 +0800

    first

---
+++ dist/js/slider.js
@@ -0,0 +1,10 @@
+var slider = tns({
+  container: ".clientReview__slider",
+  items: 1,
+  speed: 200,
+  guttter: 100,
+  slideBy: "page",
+  autoplay: true,
+  nav: false,
+  autoplayButtonOutput: false,
+});
